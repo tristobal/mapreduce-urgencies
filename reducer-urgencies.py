309c3c0acd8e1006ee2c0e7ad05805f4b5de0c36
Cristobal Sanchez <cristobal.sanchez@globant.com>
2019-06-13 19:42:54 -0400

    Modifica lógica de reducer-urgencies.py

--- reducer-urgencies.py
+++ reducer-urgencies.py
@@ -1,18 +1,22 @@
+#!/usr/bin/env python
 import sys
 
-resume = {}
-
-def putIntoResume(resume, hospital, urgencies):
-    if hospital not in resume:
-        resume[hospital] = urgencies
-    else:
-        resume[hospital] = resume[hospital] + urgencies
-
+current_hospital = None 
+count_urgencies = 0
+hospital = None
 
 for line in sys.stdin:
-    line = line.strip()
-    hospital, urgencies = line.split('\t',1)
-    putIntoResume(resume, hospital, int(urgencies))
+    line = line.strip();
+    hospital, urgencies = line.split('\t',1) 
+    urgencies = int(urgencies)
+     
+    if current_hospital == hospital:
+        count_urgencies += int(urgencies)
+    else:
+        if current_hospital:
+           print("%s\t%s" % (current_hospital, count_urgencies))
+        count_urgencies = urgencies
+        current_hospital = hospital
 
-for (x, y) in resume.items():
-    print("%s\t%s" % (x, y))
+if current_hospital == hospital:
+	print("%s\t%s" % (current_hospital, count_urgencies))
