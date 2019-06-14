216622a9785539d03b0c2af070f7b60ff8a3b394
Cristobal Sanchez <cristobal.sanchez@globant.com>
2019-06-13 20:06:18 -0400

    Elimina código comentado de reducer.

--- reducer-urgencies.py
+++ reducer-urgencies.py
@@ -16,28 +16,3 @@ for line in sys.stdin:
  
 for (x, y) in resume.items():
     print("%s\t%s" % (x, y))
-
-
-'''
-import sys
-
-current_hospital = None 
-count_urgencies = 0
-hospital = None
-
-for line in sys.stdin:
-    line = line.strip();
-    hospital, urgencies = line.split('\t',1) 
-    urgencies = int(urgencies)
-     
-    if current_hospital == hospital:
-        count_urgencies += int(urgencies)
-    else:
-        if current_hospital:
-           print("%s\t%s" % (current_hospital, count_urgencies))
-        count_urgencies = urgencies
-        current_hospital = hospital
-
-if current_hospital == hospital:
-	print("%s\t%s" % (current_hospital, count_urgencies))
-'''
